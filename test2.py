d8461369f284526f6771e5281eae95b67b12b9e9
Debojit Das <debojit666@gmail.com>
2019-09-17 20:43:37 +0200

    VER 2

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ num = 0
 for i in range(51):
     result = result + num
     num += 1
-print("The result is : ", result)
+print("THE RESULT IS : ", result)
 
 for i in range(0,16,5):
-    print(i) 
+    print(i)
